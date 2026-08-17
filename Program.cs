bac3d6f95c1908b2be9875335d2e01788f2a9bf6
Andrew Murray <andrew@ibsystems.co.uk>
2026-08-18 00:27:01 +0100

    Honour Render PORT, remove HTTPS redirect

--- Program.cs
+++ Program.cs
@@ -8,15 +8,16 @@ builder.Services.AddSingleton<NavCalculator>();
 builder.Services.AddSingleton<WeatherService>();
 builder.Services.AddScoped<ClaudeAgentService>();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.MapRazorPages();
